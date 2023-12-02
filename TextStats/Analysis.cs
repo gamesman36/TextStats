4ebf1af33da2003ed26305c8f204b241f2004ebb
gamesman36 <gamesman36@outlook.com>
2023-12-02 21:46:10 +0100

    Initial commit

---
+++ TextStats/Analysis.cs
@@ -0,0 +1,37 @@
+﻿namespace TextStats
+{
+    internal class Analysis
+    {
+        public string Text { get; private set; }
+        public string Alphabet { get; private set; }
+        public Dictionary<char, int> Counters { get; private set; }
+
+        public Analysis(string text)
+        {
+            Text = text.ToLower();
+            Alphabet = "abcdefghijklmnopqrstuvwxyz";
+            Counters = new Dictionary<char, int>();
+
+            foreach (char ch in Alphabet)
+            {
+                Counters[ch] = 0;
+            }
+        }
+
+        public void CountChars()
+        {
+            foreach (char ch in Text)
+            {
+                if (Counters.ContainsKey(ch))
+                {
+                    Counters[ch]++;
+                }
+            }
+        }
+
+        public int GetCharCount(char ch)
+        {
+            return Counters[ch];
+        }
+    }
+}
---
+++ TextStats/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TextStats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Analysis analysis = new Analysis(text);
+            analysis.CountChars();
+            
+            foreach (var pair in analysis.Counters)
+            {
+                Console.WriteLine($"Character: {pair.Key}, Count: {pair.Value}");
+            }
+        }
+    }
+}
